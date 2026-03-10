fe596b2987286c7523ac025d244eeab05d46774f
weishu <twsxtd@gmail.com>
2026-03-10 19:57:34 +0800

    drivers: kernelsu: Update KernelSU-Next [SQUASH]
    kernel: make clang-format happy
    
    kernel: refine stackprotector workaround (https://github.com/tiann/KernelSU/pull/3264)
    
    feat: add late-load mode support (https://github.com/tiann/KernelSU/pull/3258)
    
    kernel: enforcing selinux at late-load mode
    
    kernel: sepolicy: use GFP_KERNEL (https://github.com/tiann/KernelSU/pull/3271)
    
    Now that all sepolicy functions are called from a normal context, we can
    stop using GFP_ATOMIC.
    
    kernel: upgrade ddk, use ddk-min (https://github.com/tiann/KernelSU/pull/3280)
    
    - Validate `__versions` section in check_symbol to ensure lkm can be
    force loaded on some kernel.
    
    kernel: Fix RLIMIT_NPROC leak on root escape (https://github.com/tiann/KernelSU/pull/3286)
    
    After `escape_to_root`, `cred->uid` changes, but `cred->user` and
    `cred->ucounts` remain unchanged. Because `cred->user` does not change,
    `commit_creds()` does not decrement the `ucounts` associated with the
    original UID.
    
    When `execve` is executed afterwards, `cred->user` still does not
    change, but `cred->ucounts` changes. This happens because `cred->uid` is
    now different from `cred->ucounts->uid`. However, since `cred->user` is
    still unchanged, `commit_creds()` still does not decrement the original
    `ucounts`. After this point, it can never be decremented anymore,
    because `exit` will only decrease the `ucounts` of the new UID.
    
    Since the original UID’s `ucounts` increases every time `su` is
    executed, it will eventually exceed the limit until it reaches the
    `rlimit` threshold.
    
    This fixes https://github.com/tiann/KernelSU/issues/3214.
    
    Update KSU config to require EXT4_FS (https://github.com/tiann/KernelSU/pull/3266)
    
    Add dependency on EXT4_FS for KernelSU support for
    `ext4_unregister_sysfs`.
    
    Signed-off-by: shadichy <shadichy@blisslabs.org>
    Co-Authored-By: Ylarod <me@ylarod.cn>
    Co-Authored-By: Wang Han <416810799@qq.com>

--- drivers/kernelsu/supercalls.c
+++ drivers/kernelsu/supercalls.c
@@ -81,14 +81,17 @@ static int do_get_info(void __user *arg)
     struct ksu_get_info_cmd cmd = { .version = KERNEL_SU_VERSION, .flags = 0 };
 
 #ifdef MODULE
-	cmd.flags |= 0x1;
+	cmd.flags |= KSU_GET_INFO_FLAG_LKM;
 #endif
 
 	if (ksuver_override)
 	    cmd.version = ksuver_override;
 
 	if (is_manager()) {
-		cmd.flags |= 0x2;
+		cmd.flags |= KSU_GET_INFO_FLAG_MANAGER;
+	}
+	if (ksu_late_loaded) {
+		cmd.flags |= KSU_GET_INFO_FLAG_LATE_LOAD;
 	}
 	cmd.features = KSU_FEATURE_MAX;
 
@@ -113,8 +116,12 @@ static int do_report_event(void __user *arg)
 		static bool post_fs_data_lock = false;
 		if (!post_fs_data_lock) {
 			post_fs_data_lock = true;
-			pr_info("post-fs-data triggered\n");
-			on_post_fs_data();
+			if (ksu_late_loaded) {
+				pr_info("post-fs-data skipped (late load)\n");
+			} else {
+				pr_info("post-fs-data triggered\n");
+				on_post_fs_data();
+			}
 		}
 		break;
 	}
@@ -122,11 +129,15 @@ static int do_report_event(void __user *arg)
 		static bool boot_complete_lock = false;
 		if (!boot_complete_lock) {
 			boot_complete_lock = true;
-			pr_info("boot_complete triggered\n");
-			on_boot_completed();
+			if (ksu_late_loaded) {
+				pr_info("boot_complete skipped (late load)\n");
+			} else {
+				pr_info("boot_complete triggered\n");
+				on_boot_completed();
 #ifdef CONFIG_KSU_SUSFS
-			susfs_start_sdcard_monitor_fn();
+				susfs_start_sdcard_monitor_fn();
 #endif // #ifdef CONFIG_KSU_SUSFS
+			}
 		}
 		break;
 	}
